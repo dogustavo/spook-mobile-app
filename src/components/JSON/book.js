df418a2fbb26603f8a2072aca2ded229d483a280
gustavo <gustavo.oliveira@atuo.in>
2020-07-01 20:25:08 -0300

    adicionado lógica de swap

--- src/components/JSON/book.js
+++ src/components/JSON/book.js
@@ -8,6 +8,7 @@ const BOOK_DATA = [
         publisher: 'Vaga Lume',
         photo: 'https://56e9af4bb89f1d73465cbd39.static-01.com/l/images/27c814c3d7a473f763a886d6d89275761bb54c65.jpeg',
         like: true,
+        distance: '2km'
         
     },
     {
@@ -19,6 +20,7 @@ const BOOK_DATA = [
         publisher: 'Vaga Lume',
         photo: 'https://1.bp.blogspot.com/-igVDnXNDCIo/VaO7UjmCbII/AAAAAAAAFu4/hI0_DVxsMRI/s1600/fotos7%2B602.jpg',
         like: true,
+        distance: '8km'
         
     },
     {
@@ -30,6 +32,7 @@ const BOOK_DATA = [
         publisher: 'Vaga Lume',
         photo: 'https://photos.enjoei.com.br/public/1200xN/czM6Ly9waG90b3MuZW5qb2VpLmNvbS5ici9wcm9kdWN0cy8xMTYwNjMwOC9lYjUwZjBkY2M4MDJjNzRiMTNhODJiZGExMzJlMzQyZC5qcGc',
         like: true,
+        distance: '4km'
         
     },
     {
@@ -41,6 +44,7 @@ const BOOK_DATA = [
         publisher: 'Vaga Lume',
         photo: 'https://photos.enjoei.com.br/public/1200xN/czM6Ly9waG90b3MuZW5qb2VpLmNvbS5ici9wcm9kdWN0cy82NTMyOTIvZjQwMDM4OTgxNjFkOTQzNjc0NDEzOWY4ZTJjZmUzOWMuanBn',
         like: true,
+        distance: '9Km'
         
     },
 ]
--- src/pages/bookRegister/bookList/index.js
+++ src/pages/bookRegister/bookList/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, Image, Animated, Dimensions, PanResponder } from 'react-native';
 
 import Screen from '../../../components/screen';
 import Header from '../../../components/header';
@@ -7,8 +7,61 @@ import Header from '../../../components/header';
 import styles from './style';
 
 import booksData from '../../../components/JSON/book';
+import { transform } from '@babel/core';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+const position = new Animated.ValueXY();
 
 const BookList = () => {
+
+    const [ like, setLike ] = useState([]);
+    const [ dislike, setDislike ] = useState([]);
+
+    const [ current, setCurrent ] = useState(0);
+    const [ done, setDone ] = useState(false);
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder:(evt, gestureState) => true,
+        onPanResponderMove:(evt, gestureState) => {
+            position.setValue({ x: gestureState.dx, y: gestureState.dy });
+        } ,
+        onPanResponderRelease:(evt, gestureState) => {
+            if(
+                position.x._value < -50 && 
+                position.y._value > SCREEN_HEIGHT / 2 - 150 && 
+                position.y._value < SCREEN_HEIGHT / 2 
+            ) {
+                Animated.spring(position, {
+                    toValue: { x: -SCREEN_WIDTH - 10, y: position.y._value},
+                    friction: 10,
+                }).start(() => {
+                    setLike([...like, booksData[current].name]);
+                    if( current == booksData.length - 1 ) { 
+                        setDone(true);
+                        position.setValue({x: 0, y: 0})
+                    }
+                })
+            }
+            else if(
+                position.x._value > 50 &&
+                position.y._value > SCREEN_HEIGHT / 2 - 150 &&
+                position.y._value < SCREEN_HEIGHT / 2
+            ) {
+                Animated.spring(position, {
+                    toValue: { x: -SCREEN_WIDTH + 10, y: position.y._value},
+                    friction: 10,
+                }).start(() => {
+                    setDislike([...like, booksData[current].name]);
+                    if( current == booksData.length - 1 ) { 
+                        setDone(true);
+                        position.setValue({x: 0, y: 0})
+                    }
+                })
+            }
+        }
+    })
+
 
     return (
         <Screen color="#FFF">
@@ -22,7 +75,14 @@ const BookList = () => {
                     {
                         booksData.map((book, index) => (
                             
-                            <View key={book.id} style={{...styles.card, zIndex: booksData.length - index}}>
+                            <Animated.View 
+                                key={book.id} 
+                                // {...panResponder.panHandlers}
+                                style={{
+                                    ...styles.card,
+                                    // transform: position.getTranslateTransform()
+                                }}
+                            >
                                 <Image
                                     source={{uri: `${book.photo}`}}
                                     style={styles.bookPhoto}
@@ -31,9 +91,9 @@ const BookList = () => {
                                 <View style={styles.wrapBookName}>
                                     <Text style={styles.bookName}>{book.name}</Text>
                                 </View>
-                            </View>
+                            </Animated.View>
                             
-                        ))
+                        )).reverse()
                     }  
                 </View>
             </View>
